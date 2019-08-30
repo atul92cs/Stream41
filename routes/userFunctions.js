043369f383c283692ea65e480b8d530dff55401a
Atul <atul.11192@gmail.com>
2019-08-30 06:57:00 +0530

    adding styles and functionality

--- routes/userFunctions.js
+++ routes/userFunctions.js
@@ -1,8 +1,8 @@
 const routes=require('./index');
 module.exports=function(app,passport){
-    app.post('/signin',passport.authenticate('local-signin',{successRedirect:'/dashboard',failureRedirect:'/signin'
-}));
-    app.post('/signup',passport.authenticate('local-signup',{successRedirect:'/',failureRedirect:'/signup',session:false}));
+    app.post('/signin',passport.authenticate('local-signin',{successRedirect:'/dashboard',failureRedirect:'/signin',failureFlash:true}
+));
+    app.post('/signup',passport.authenticate('local-signup',{successRedirect:'/',failureRedirect:'/signup',session:false,failureFlash:true}));
     app.get('/broadcast',isLoggedin,routes.broadcast);
     app.get('/watch',isLoggedin,routes.watch);
     app.get('/dashboard',isLoggedin,routes.dashboard);
